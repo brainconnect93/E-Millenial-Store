0982c6872d434033a5b49f1ca3b22d2488e0ec80
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-01 02:13:07 +0100

    setup the webpack environment

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: 'svg-inline-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin(
+    {
+      template: path.resolve(__dirname, './src/index.html'),
+    },
+
+  )],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+};
